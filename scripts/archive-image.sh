5ab7b22806414ae74e458dde9919450a71ee845a
Javier Marcet <javier@keezel.co>
2019-06-11 16:05:15 +0200

    archive-image.sh: copy all packages
    Signed-off-by: Javier Marcet <javier@keezel.co>

--- scripts/archive-image.sh
+++ scripts/archive-image.sh
@@ -7,22 +7,29 @@ set -v
 (
 _target=$( grep '^CONFIG_TARGET_[a-z0-9]\+=y' .config | sed -e 's:^CONFIG_TARGET_\([a-z0-9]\+\)=y:\1:' )
 _bindir=$( ls -d bin/targets/$_target/* )
+_bindir2=bin/packages
 
 _destdir=$DESTDIR/${_bindir/bin\/targets\/}
+_destdir2=$DESTDIR/$_target/packages
 _img=$( ls -t $_bindir/*-factory.img | head -1 )
 _date=$( date -r $_img +%Y%m%d_%H%M%S )
 _subdir=$_date-`echo $_img | cut -d'-' -f3-4`
 
-_t=$_destdir/$_subdir
+_t1=$_destdir/$_subdir
+_t2=$_destdir2/$_subdir
 
-if [ ! -d "$_t" ]; then
-    mkdir -p $_t
-    for i in $( ls -t $_bindir/ | head -6 ); do
-        cp -a -v $_bindir/$i $_t/
-    done
-else
-    echo $_t already exists
+if [ -d "$_t1" ] || [ -d "$_t2" ]; then
+    [ -d "$_t1" ] && echo $_t1 already exists
+    [ -d "$_t2" ] && echo $_t2 already exists
+    exit 1
 fi
+
+mkdir -p $_t1
+for i in $( ls -t $_bindir/ | head -6 ); do
+    cp -a -v $_bindir/$i $_t1/
+done
+
+cp -a $_bindir2 $_t2
 )
 
 set +v
