0c620f3c4efb4679dbf7232c9c724f24027ea760
Javier Marcet <javier@keezel.co>
2019-06-11 16:49:56 +0200

    files: source /etc/profile in all tmux windows, add new entries to bash history
    Signed-off-by: Javier Marcet <javier@keezel.co>

--- files/etc/tmux-layouts/stats.window.sh
+++ files/etc/tmux-layouts/stats.window.sh
@@ -15,16 +15,16 @@ select_pane 2
 split_h 50
 select_pane 3
 
-run_cmd "eval \`resize\`" 0
-run_cmd "cler" 0
+run_cmd ". /etc/profile" 0
+run_cmd "clear" 0
 run_cmd "htop" 0
-run_cmd "eval \`resize\`" 1
+run_cmd ". /etc/profile" 1
 run_cmd "logread" 1
 run_cmd "logread -f" 1
-run_cmd "eval \`resize\`" 2
+run_cmd ". /etc/profile" 2
 run_cmd "clear" 2
 run_cmd "iperf3 -s" 2
-run_cmd "eval \`resize\`" 3
+run_cmd ". /etc/profile" 3
 run_cmd "clear" 3
 
 ## Set active pane.
