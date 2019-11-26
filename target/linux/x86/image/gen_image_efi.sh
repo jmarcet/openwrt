007eb4b57f78f8c74632a6fa317daa0ce169badb
Javier Marcet <javier@marcet.info>
2019-11-26 14:18:15 +0100

    x86: add dual boot suppport to x86-64 uefi, drop hybrid mode
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- target/linux/x86/image/gen_image_efi.sh
+++ target/linux/x86/image/gen_image_efi.sh
@@ -1,44 +1,44 @@
 #!/usr/bin/env bash
 set -x
-[ $# == 8 -o $# == 9 ] || {
-    echo "SYNTAX: $0 <file> <kernel size> <kernel directory> <efi size> <efi image> <efigrubmodule size> <rootfs size> <rootfs image> [<align>]"
+[ $# == 7 ] || {
+    echo "SYNTAX: $1 <file> <efi size> <efi image> <kernel size> <kernel directory> <rootfs size> <rootfs image>"
     exit 1
 }
 
 OUTPUT="$1"
-KERNELSIZE="$2"
-KERNELDIR="$3"
-EFISIZE="$4"
-EFIIMAGE="$5"
-EFIGRUBSIZE="$6"
-ROOTFSSIZE="$7"
-ROOTFSIMAGE="$8"
-ALIGN="$9"
+EFISIZE="$2"
+EFIIMAGE="$3"
+KERNELSIZE="$4"
+KERNELDIR="$5"
+ROOTFSSIZE="$6"
+ROOTFSIMAGE="$7"
+GPTOFFSET=$(( $ROOTFSSIZE + 33 ))
 
 rm -f "$OUTPUT"
 
 head=16
 sect=63
-cyl=$(( ($KERNELSIZE + $EFISIZE + $EFIGRUBSIZE + $ROOTFSSIZE) * 1024 * 1024 / ($head * $sect * 512) ))
+cyl=$(( ($EFISIZE + $KERNELSIZE + 2 * $ROOTFSSIZE) * 1024 * 1024 / ($head * $sect * 512)))
 
 # create partition table
-set `ptgen -o "$OUTPUT" -h $head -s $sect -p ${KERNELSIZE}m -p ${EFISIZE}m -p ${EFIGRUBSIZE}m -p ${ROOTFSSIZE}m ${ALIGN:+-l $ALIGN} ${IMG_PART_SIGNATURE:+-S 0x$IMG_PART_SIGNATURE}`
+set `ptgen -o "$OUTPUT" -l 256 -h $head -s $sect -p${EFISIZE}m -p${KERNELSIZE}m -p${ROOTFSSIZE}m -p${ROOTFSSIZE}m ${SIGNATURE:+-S 0x$SIGNATURE}`
 
-KERNELOFFSET="$(($1 / 512))"
-KERNELSIZE="$2"
-EFIOFFSET="$(($3 / 512))"
-EFISIZE="$(($4 / 512))"
-EFIGRUBOFFSET="$(($5 / 512))"
-EFIGRUBSIZE="$(($6 / 512))"
-ROOTFSOFFSET="$(($7 / 512))"
-ROOTFSSIZE="$(($8 / 512))"
+EFIOFFSET="$(($1 / 512))"
+KERNELOFFSET="$(($3 / 512))"
+KERNELSIZE="$4"
+ROOTFSOFFSET="$(($5 / 512))"
+ROOTFSSIZE="$(($6 / 512))"
 
-dd if=/dev/zero of="$OUTPUT" bs=512 seek="$ROOTFSOFFSET" conv=notrunc count="$ROOTFSSIZE"
+dd if=/dev/zero of="$OUTPUT" bs=512 seek="$ROOTFSOFFSET" conv=notrunc count="$(( 2 * $ROOTFSSIZE ))"
 dd if="$ROOTFSIMAGE" of="$OUTPUT" bs=512 seek="$ROOTFSOFFSET" conv=notrunc
 dd if="$EFIIMAGE" of="$OUTPUT" bs=512 seek="$EFIOFFSET" conv=notrunc
-
-[ -n "$NOGRUB" ] && exit 0
 
 make_ext4fs -J -l "$KERNELSIZE" "$OUTPUT.kernel" "$KERNELDIR"
 dd if="$OUTPUT.kernel" of="$OUTPUT" bs=512 seek="$KERNELOFFSET" conv=notrunc
 rm -f "$OUTPUT.kernel"
+
+# Convert the MBR partition to GPT and set partition types and labels
+dd if=/dev/zero of="$OUTPUT" bs=512 count="$GPTOFFSET" conv=notrunc oflag=append
+sgdisk -g "$OUTPUT"
+sgdisk -t 1:ef00 -t 2:ea00 -c 2:openwrt_boot -c 3:openwrt_rootfs -c 4:openwrt_rootfs_alt "$OUTPUT"
+sgdisk -u 3:"$EFI_SIGNATURE" "$OUTPUT"
