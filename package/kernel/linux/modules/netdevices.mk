64828a50e2f33235976ddc70e2e999c1e1c38060
Javier Marcet <javier@marcet.info>
2019-12-01 18:04:34 +0100

    kernel: add ipvtap module for docker-ce
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- package/kernel/linux/modules/netdevices.mk
+++ package/kernel/linux/modules/netdevices.mk
@@ -901,6 +901,27 @@ endef
 $(eval $(call KernelPackage,ipvlan))
 
 
+define KernelPackage/ipvtap
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  TITLE:=IP-VLAN TAP support
+  DEPENDS:=+kmod-ipvlan
+  KCONFIG:=CONFIG_IPVTAP
+  FILES:= \
+    $(LINUX_DIR)/drivers/net/tap.ko \
+    $(LINUX_DIR)/drivers/net/ipvlan/ipvtap.ko
+  AUTOLOAD:=$(call AutoProbe,ipvtap)
+endef
+
+define KernelPackage/ipvtap/description
+ This adds a specialized tap character device driver that is based
+ on the IP-VLAN network interface, called ipvtap. An ipvtap device
+ can be added in the same way as a ipvlan device, using 'type
+ ipvtap', and then be accessed through the tap user space interface.
+endef
+
+$(eval $(call KernelPackage,ipvtap))
+
+
 define KernelPackage/tulip
   TITLE:=Tulip family network device support
   DEPENDS:=@PCI_SUPPORT +kmod-mii
