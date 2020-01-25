4103ad9a680ae5fdd74de8ab2cb5269138b4e40a
Javier Marcet <javier@marcet.info>
2020-01-25 18:07:32 +0100

    scripts/archive-image.sh: adapt to x86-64 platform
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- scripts/archive-image.sh
+++ scripts/archive-image.sh
@@ -7,29 +7,24 @@ set -v
 (
 _target=$( grep '^CONFIG_TARGET_[a-z0-9]\+=y' .config | sed -e 's:^CONFIG_TARGET_\([a-z0-9]\+\)=y:\1:' )
 _bindir=$( ls -d bin/targets/$_target/* )
-_bindir2=bin/packages
+_bindir2=$_bindir/packages
 
 _destdir=$DESTDIR/${_bindir/bin\/targets\/}
-_destdir2=$DESTDIR/$_target/packages
-_img=$( ls -t $_bindir/*-factory.img | head -1 )
+_img=$( ls -t $_bindir/*-x86-64-uefi-gpt-squashfs.img | head -1 )
 _date=$( date -r $_img +%Y%m%d_%H%M%S )
 _subdir=$_date-`echo $_img | cut -d'-' -f3-4`
 
 _t1=$_destdir/$_subdir
-_t2=$_destdir2/$_subdir
 
-if [ -d "$_t1" ] || [ -d "$_t2" ]; then
-    [ -d "$_t1" ] && echo $_t1 already exists
-    [ -d "$_t2" ] && echo $_t2 already exists
+if [ -d "$_t1" ]; then
+    echo "$_t1 already exists"
     exit 1
 fi
 
 mkdir -p $_t1
-for i in $( ls -t $_bindir/ | head -6 ); do
-    cp -a -v $_bindir/$i $_t1/
+for dir in $( ls -t $_bindir/ | head -10 ); do
+    cp -a -v $_bindir/$dir $_t1/
 done
-
-cp -a $_bindir2 $_t2
 )
 
 set +v
