e227911baca1f3bb752a5de2714e321ff71e5865
Javier Marcet <javier@marcet.info>
2020-01-25 18:07:32 +0100

    scripts/archive-image.sh: adapt to x86-64 platform
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- scripts/archive-image.sh
+++ scripts/archive-image.sh
@@ -7,20 +7,19 @@ set -v
 (
 _target=$( grep '^CONFIG_TARGET_[a-z0-9]\+=y' .config | sed -e 's:^CONFIG_TARGET_\([a-z0-9]\+\)=y:\1:' )
 _bindir=$( ls -d bin/targets/$_target/* )
-_bindir2=bin/packages
+_bindir2=$_bindir/packages
 
 _destdir=$DESTDIR/${_bindir/bin\/targets\/}
-_destdir2=$DESTDIR/$_target/packages
-_img=$( ls -t $_bindir/*-factory.img | head -1 )
+_img=$( ls -t $_bindir/*-x86-64-uefi-gpt-squashfs.img | head -1 )
 _date=$( date -r $_img +%Y%m%d_%H%M%S )
 _subdir=$_date-`echo $_img | cut -d'-' -f3-4`
 
 _t1=$_destdir/$_subdir
-_t2=$_destdir2/$_subdir
+_t2=$_t1/packages
 
 if [ -d "$_t1" ] || [ -d "$_t2" ]; then
-    [ -d "$_t1" ] && echo $_t1 already exists
-    [ -d "$_t2" ] && echo $_t2 already exists
+    [ -d "$_t1" ] && echo "$_t1 already exists"
+    [ -d "$_t2" ] && echo "$_t2 already exists"
     exit 1
 fi
 
