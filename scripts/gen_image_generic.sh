a64e0babae17ac57a6b52597c6a5022e0cf54a84
Javier Marcet <javier@marcet.info>
2020-04-02 18:38:01 +0200

    x86: drop hybrid BIOS mode from EFI images, add dualboot in the process
    With this in place a native EFI system will be able to sysupgrade
    a running system, plus it will keep the previous image which is easy to
    switch to in case of a problematic upgrade.
    
    We change the GUID types of the partitions. We drop the BIOS_BOOT
    partition, add a new FREEDESKTOP_BOOT GUID with PARTLABEL=openwrt_boot
    to hold the kernels (vmlinuz and vmlinuz_alt), change GUID_PARTITION_BASIC_DATA
    to GUID_PARTITION_LINUX_FILESYSTEM with PARTLABEL=openwrt_rootfs and
    PARTLABEL=openwrt_rootfs_alt for the main and alternate rootfs.
    
    We enable the alternate partition table, this fixes the problems with
    the combined-efi.img detected by gdisk.
    
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- scripts/gen_image_generic.sh
+++ scripts/gen_image_generic.sh
@@ -17,25 +17,26 @@ rm -f "$OUTPUT"
 
 head=16
 sect=63
-cyl=$(( (KERNELSIZE + ROOTFSSIZE) * 1024 * 1024 / (head * sect * 512)))
 
 # create partition table
-set $(ptgen -o "$OUTPUT" -h $head -s $sect ${GUID:+-g} -p ${KERNELSIZE}m -p ${ROOTFSSIZE}m ${ALIGN:+-l $ALIGN} ${SIGNATURE:+-S 0x$SIGNATURE} ${GUID:+-G $GUID})
-
-KERNELOFFSET="$(($1 / 512))"
-KERNELSIZE="$2"
-ROOTFSOFFSET="$(($3 / 512))"
-ROOTFSSIZE="$(($4 / 512))"
-
-[ -n "$PADDING" ] && dd if=/dev/zero of="$OUTPUT" bs=512 seek="$ROOTFSOFFSET" conv=notrunc count="$ROOTFSSIZE"
-dd if="$ROOTFSIMAGE" of="$OUTPUT" bs=512 seek="$ROOTFSOFFSET" conv=notrunc
+set $(ptgen -o "$OUTPUT" -h $head -s $sect ${GUID:+-g -p 1m} -p ${KERNELSIZE}m -p ${ROOTFSSIZE}m ${GUID:+-p ${ROOTFSSIZE}m} ${ALIGN:+-l $ALIGN} ${SIGNATURE:+-S 0x$SIGNATURE} ${GUID:+-G $GUID})
 
 if [ -n "$GUID" ]; then
-    [ -n "$PADDING" ] && dd if=/dev/zero of="$OUTPUT" bs=512 seek="$(($ROOTFSOFFSET + $ROOTFSSIZE))" conv=notrunc count="$sect"
-    mkfs.fat -n kernel -C "$OUTPUT.kernel" -S 512 "$(($KERNELSIZE / 1024))"
-    mcopy -s -i "$OUTPUT.kernel" "$KERNELDIR"/* ::/
+    KERNELOFFSET="$(($3 / 512))"
+    KERNELSIZE="$4"
+    ROOTFSOFFSET="$(($5 / 512))"
+    ROOTFSSIZE="$(($6 / 512))"
 else
-    make_ext4fs -J -L kernel -l "$KERNELSIZE" "$OUTPUT.kernel" "$KERNELDIR"
+    KERNELOFFSET="$(($1 / 512))"
+    KERNELSIZE="$2"
+    ROOTFSOFFSET="$(($3 / 512))"
+    ROOTFSSIZE="$(($4 / 512))"
 fi
+
+[ -n "$PADDING" ] && dd if=/dev/zero of="$OUTPUT" bs=512 seek="$ROOTFSOFFSET" conv=notrunc count="$ROOTFSSIZE"
+[ -n "$GUID" ] && dd if="$OUTPUT.grub.img" of="$OUTPUT" bs=512 seek=512 conv=notrunc count=2048
+dd if="$ROOTFSIMAGE" of="$OUTPUT" bs=512 seek="$ROOTFSOFFSET" conv=notrunc
+
+make_ext4fs -J -L kernel -l "$KERNELSIZE" "$OUTPUT.kernel" "$KERNELDIR"
 dd if="$OUTPUT.kernel" of="$OUTPUT" bs=512 seek="$KERNELOFFSET" conv=notrunc
 rm -f "$OUTPUT.kernel"
