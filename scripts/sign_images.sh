897f6698e78c72e7defe7938faf3f2f9f9ffbaf1
Javier Marcet <javier@marcet.info>
2020-04-05 10:15:02 +0200

    build: sign_images.sh: changes for local testing
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- scripts/sign_images.sh
+++ scripts/sign_images.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -ex
+
 # directory where search for images
 TOP_DIR="${TOP_DIR:-./bin/targets}"
 # key to sign images
@@ -9,19 +11,18 @@ REMOVE_OTER_SIGNATURES="${REMOVE_OTER_SIGNATURES:-1}"
 
 # find all sysupgrade images in TOP_DIR
 # factory images don't need signatures as non OpenWrt system doen't check them anyway
-for image in $(find $TOP_DIR -type f -name "*-sysupgrade.bin"); do
-	# check if image actually support metadata
-	if fwtool -i /dev/null "$image"; then
-		# remove all previous signatures
-		if [ -n "$REMOVE_OTER_SIGNATURES" ]; then
-			while [ "$?" = 0 ]; do
-				fwtool -t -s /dev/null "$image"
-			done
-		fi
-		# run same operation as build root does for signing
-		cp "$BUILD_KEY.ucert" "$image.ucert"
-		usign -S -m "$image" -s "$BUILD_KEY" -x "$image.sig"
-		ucert -A -c "$image.ucert" -x "$image.sig"
-		fwtool -S "$image.ucert" "$image"
+#for image in $(find $TOP_DIR -type f -name "*-sysupgrade.bin" -o -name "*-combined*.img*"); do
+for image in $(find $TOP_DIR -type f -name "*-combined-efi.img" -o -name "*-combined-efi.img.gz"); do
+	# remove all previous signatures
+	if [ -n "$REMOVE_OTER_SIGNATURES" ]; then
+		while true; do
+			fwtool -t -s /dev/null "$image" || break
+		done
 	fi
+	# run same operation as build root does for signing
+	#cp -f "$BUILD_KEY.ucert" "$image.ucert"
+	[ -e "$image.ucert" ] && rm -f $image.ucert
+	usign -S -m "$image" -s "$BUILD_KEY" -x "$image.sig"
+	ucert -A -c "$image.ucert" -x "$image.sig"
+	fwtool -S "$image.ucert" "$image"
 done
