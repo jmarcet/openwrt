0ec974073b3ea7263d98fa8a4dc8a93f49399755
Javier Marcet <javier@marcet.info>
2020-04-05 10:13:39 +0200

    x86: add new subdevice asrock-z77-pro4-m
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- target/linux/x86/image/64.mk
+++ target/linux/x86/image/64.mk
@@ -1,5 +1,18 @@
 define Device/generic
   DEVICE_TITLE := Generic x86/64
   GRUB2_VARIANT := generic
+  DEVICE_PACKAGES += kmod-e1000e kmod-e1000 kmod-r8169 kmod-igb kmod-bnx2
+  SUPPORTED_DEVICES := generic
 endef
 TARGET_DEVICES += generic
+
+define Device/asrock-z77-pro4-m
+  DEVICE_TITLE := ASRock Z77 Pro4-M
+  DEVICE_PACKAGES += kmod-fs-btrfs kmod-hwmon-drivetemp kmod-hwmon-nct6775 \
+	kmod-itco-wdt kmod-kvm-intel kmod-md-mod kmod-md-raid456 kmod-phy-realtek \
+	kmod-r8169 kmod-sound-hda-codec-hdmi kmod-sound-hda-codec-realtek \
+	kmod-usb-net-asix-ax88179 kmod-veth
+  IMAGES := combined-efi.img.gz
+  SUPPORTED_DEVICES := asrock-z77-pro4-m
+endef
+TARGET_DEVICES += asrock-z77-pro4-m
