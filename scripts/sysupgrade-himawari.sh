c4e6a0c9a5175d6c7a082d25b30b6cd7c0a526f1
Javier Marcet <javier@marcet.info>
2020-04-06 20:58:47 +0200

    scripts/sysupgrade-himawari.sh: add script to launch sysupgrade on the running x86 efi system with the newest build
    Signed-off-by: Javier Marcet <javier@marcet.info>

---
+++ scripts/sysupgrade-himawari.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+_target=$( grep '^CONFIG_TARGET_[a-z0-9]\+=y' .config | sed -e 's:^CONFIG_TARGET_\([a-z0-9]\+\)=y:\1:' )
+_bindir=$( ls -d bin/targets/$_target/* )
+#_arch=$( echo $_bindir | cut -d'/' -f3- | tr '/' '-' )
+
+if [ ! -w /boot/vmlinuz -o ! -w /boot/vmlinuz_alt ]; then
+    echo "Can't write kernel"
+    exit 1
+fi
+
+_image=$( ls -t $_bindir/openwrt-*-x86-64-asrock-z77-pro4-m-squashfs-combined-efi.img.gz | head -1 )
+if [ -z "$_image" ]; then
+    echo "Usage: no suitable image found"
+    exit 1
+fi
+
+sysupgrade -i -n -p -v $_image
+
+exit 0
