9272b9ec23b849692c619fddbf8fd256799428cd
Javier Marcet <javier@marcet.info>
2020-12-26 15:16:07 +0100

    build: fix signing logic and drop copying core ucert, or image signature check fails
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- include/image-commands.mk
+++ include/image-commands.mk
@@ -72,14 +72,14 @@ metadata_json = \
 
 define Build/append-metadata
 	$(if $(SUPPORTED_DEVICES),-echo $(call metadata_json) | fwtool -I - $@)
-	[ -z "$(SIGNED_IMAGES)" \
-		-o ! -s "$(BUILD_KEY)" \
-		-o ! -s "$(BUILD_KEY).ucert" \
-		-o ! -s "$@" ] || { \
-			cp "$(BUILD_KEY).ucert" "$@.ucert" ;\
+	[ -n "$(CONFIG_SIGNED_IMAGES)" \
+		-a -s "$(BUILD_KEY)" \
+		-a -s "$(BUILD_KEY).ucert" \
+		-a -s "$@" ] && { \
 			usign -S -m "$@" -s "$(BUILD_KEY)" -x "$@.sig" ;\
 			ucert -A -c "$@.ucert" -x "$@.sig" ;\
 			fwtool -S "$@.ucert" "$@" ;\
+			rm -f "$@.ucert" ;\
 	}
 endef
 
