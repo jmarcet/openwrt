268dd968bc8ab162edb18b085553e61c0d72f3b2
Javier Marcet <javier@marcet.info>
2021-01-10 19:46:43 +0100

    kernel: bump 5.4 to 5.4.88
    Build system: x86_64
    Build-tested: x86_64
    Run-tested: x86_64
    
    No dmesg regressions, everything functional
    
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,9 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-5.4 = .87
+LINUX_VERSION-5.4 = .88
 
-LINUX_KERNEL_HASH-5.4.87 = 6a34e93e2e84bb645155124962307ad9d3646124f43838d087209ed4ea595c31
+LINUX_KERNEL_HASH-5.4.88 = v4b8007c126c7146f020230fe19ce9924ba3b089c228002dea3ef54abba1824b2
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
