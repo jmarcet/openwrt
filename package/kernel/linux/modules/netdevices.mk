2ddf754d1b91b580088f59795a1594141ced8d78
Javier Marcet <javier@marcet.info>
2021-03-08 17:27:12 +0100

    kernel: set r8169 load priority to 34, just before igb so onboard lan loads before igb
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- package/kernel/linux/modules/netdevices.mk
+++ package/kernel/linux/modules/netdevices.mk
@@ -958,7 +958,7 @@ define KernelPackage/r8169
     CONFIG_R8169 \
     CONFIG_R8169_LEDS=y
   FILES:=$(LINUX_DIR)/drivers/net/ethernet/realtek/r8169.ko
-  AUTOLOAD:=$(call AutoProbe,r8169)
+  AUTOLOAD:=$(call AutoLoad,34,r8169)
 endef
 
 define KernelPackage/r8169/description
