e1c677d523379cdfb46c507ddd4f735954b3310f
Javier Marcet <javier@marcet.info>
2021-07-08 21:48:39 +0000

    scripts/sysupgrade-himawari.sh: update /boot handling here too
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- scripts/sysupgrade-himawari.sh
+++ scripts/sysupgrade-himawari.sh
@@ -6,9 +6,12 @@ _target=$( grep '^CONFIG_TARGET_[a-z0-9]\+=y' .config | sed -e 's:^CONFIG_TARGET
 _bindir=$( ls -d bin/targets/$_target/* )
 #_arch=$( echo $_bindir | cut -d'/' -f3- | tr '/' '-' )
 
-if [ ! -w /boot/vmlinuz -o ! -w /boot/vmlinuz_alt ]; then
-    echo "Can't write kernel"
-    exit 1
+if grep -q ' /boot ' /proc/mounts; then
+    [ -w /boot ] || mount -o remount,rw /boot
+    [ -w /boot ] || {
+        echo "Boot partition mounted ro. Umount it or make it rw!"
+        exit 1
+    }
 fi
 
 _image=$( ls -t $_bindir/openwrt-*-x86-64-asustek-computer-inc-sabertooth-z77-squashfs-combined-efi.img.gz | head -1 )
