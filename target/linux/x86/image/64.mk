30a9dc9874613416af648a2b04fa436b968479ad
Javier Marcet <javier@marcet.info>
2021-07-27 20:56:22 +0200

    kernel: x86_64: drop spi deps
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- target/linux/x86/image/64.mk
+++ target/linux/x86/image/64.mk
@@ -6,7 +6,7 @@ define Device/generic
   KERNEL_INSTALL :=
   DEVICE_PACKAGES += amd64-microcode amdgpu-firmware intel-microcode \
 	kmod-drm-amdgpu kmod-e1000 kmod-e1000e kmod-itco-wdt kmod-kvm-amd \
-	kmod-kvm-intel kmod-mmc kmod-mmc-spi kmod-r8169 kmod-sound-hda-intel \
+	kmod-kvm-intel kmod-mmc kmod-r8169 kmod-sound-hda-intel \
 	kmod-sound-hda-codec-hdmi kmod-sound-hda-codec-realtek \
 	openwrt-keyring r8169-firmware
   IMAGES := combined-efi.img.gz
@@ -18,8 +18,8 @@ define Device/qemu-standard-pc-q35-ich9-2009
   GRUB_CONSOLE_CMDLINE := console=hvc0 console=tty0
   KERNEL_INSTALL :=
   DEVICE_PACKAGES += intel-microcode kmod-9pnet kmod-e1000 kmod-fs-9p \
-	kmod-mmc kmod-mmc-realtek kmod-mmc-spi \
-	kmod-sound-hda-intel openwrt-keyring virtio-console-helper
+	kmod-mmc kmod-mmc-realtek kmod-sound-hda-intel openwrt-keyring \
+	virtio-console-helper
   IMAGES := combined-efi.img.gz
   SUPPORTED_DEVICES := qemu-standard-pc-q35-ich9-2009
 endef
@@ -30,7 +30,7 @@ define Device/dell-inc-0hwtmh
   KERNEL_INSTALL :=
   DEVICE_PACKAGES += collectd-mod-wireless hostapd-common hostapd-utils \
 	kmod-cfg80211 kmod-iwlwifi kmod-mac80211 kmod-mmc kmod-mmc-realtek \
-	kmod-mmc-spi kmod-rfkill kmod-sound-hda-intel kmod-sound-hda-codec-hdmi \
+	kmod-kmod-rfkill kmod-sound-hda-intel kmod-sound-hda-codec-hdmi \
 	kmod-sound-hda-codec-realtek iwinfo iwlwifi-firmware-iwl9260 \
 	wireless-regdb wireless-tools wpa-cli wpad-openssl
   IMAGES := combined-efi.img.gz
