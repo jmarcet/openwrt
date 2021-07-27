0079af5e54ea04ffce378383b01bd9fb2a8b8d78
Javier Marcet <javier@marcet.info>
2021-07-27 19:37:05 +0200

    kernel: set ixgbe/ixgbevf, igb/igbvf & e1000e in increasing autoloading order so that our wan is in eht0, the t4 card eth1-4, and the onboard lan, when enabled, eth5
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- package/kernel/linux/modules/netdevices.mk
+++ package/kernel/linux/modules/netdevices.mk
@@ -636,7 +636,7 @@ define KernelPackage/e1000e
   DEPENDS:=@PCIE_SUPPORT +kmod-ptp
   KCONFIG:=CONFIG_E1000E
   FILES:=$(LINUX_DIR)/drivers/net/ethernet/intel/e1000e/e1000e.ko
-  AUTOLOAD:=$(call AutoProbe,e1000e)
+  AUTOLOAD:=$(call AutoProbe,36,e1000e)
   MODPARAMS.e1000e:= \
     IntMode=1 \
     InterruptThrottleRate=4,4,4,4,4,4,4,4
@@ -694,7 +694,7 @@ define KernelPackage/ixgbe
     CONFIG_IXGBE_HWMON=y \
     CONFIG_IXGBE_DCA=n
   FILES:=$(LINUX_DIR)/drivers/net/ethernet/intel/ixgbe/ixgbe.ko
-  AUTOLOAD:=$(call AutoLoad,35,ixgbe)
+  AUTOLOAD:=$(call AutoLoad,34,ixgbe)
 endef
 
 define KernelPackage/ixgbe/description
@@ -713,7 +713,7 @@ define KernelPackage/ixgbevf
     CONFIG_IXGBE_HWMON=y \
     CONFIG_IXGBE_DCA=n
   FILES:=$(LINUX_DIR)/drivers/net/ethernet/intel/ixgbevf/ixgbevf.ko
-  AUTOLOAD:=$(call AutoLoad,35,ixgbevf)
+  AUTOLOAD:=$(call AutoLoad,34,ixgbevf)
 endef
 
 define KernelPackage/ixgbevf/description
