52feb57080f9fade8e145dca102f1a6ef6813aac
Ansuel Smith <ansuelsmth@gmail.com>
2021-11-04 23:22:50 +0100

    generic: add support for 5.15
    Add hash and enable kernel 5.15 for openwrt.
    
    Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -8,9 +8,11 @@ endif
 
 LINUX_VERSION-5.4 = .158
 LINUX_VERSION-5.10 = .78
+LINUX_VERSION-5.15 = .1
 
 LINUX_KERNEL_HASH-5.4.158 = 6e018fecdc8fc24553756e582d83b82d65b10a6b03ef36262a24911f839b8d59
 LINUX_KERNEL_HASH-5.10.78 = be806c98e222ea581530727a8e83b0b96fcd678afd12944eb530e58776a6050f
+LINUX_KERNEL_HASH-5.15.1 = 32fdcd33c8ac571b9a7a297f33860f6171327961f2a2ea6bd54bf82275b614c8
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
