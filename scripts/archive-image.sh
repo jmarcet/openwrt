e572fde68b4567cf1edba50758e7f45920f15157
Javier Marcet <javier@marcet.info>
2023-04-18 16:29:24 +0200

    scripts/archive-image.sh: rework for shinnosuke, drop public builds
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- scripts/archive-image.sh
+++ scripts/archive-image.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-DESTDIR="/storage/srv/openwrt"
 ARCHDIR="/mnt/openwrt_backups"
-ARCHIVE="asrock-z77-pro4-m\|asustek-computer-inc-sabertooth-z77\|dell-inc-0hwtmh\|qemu-standard-pc-q35-ich9-2009"
 
 INTERACTIVE=1
-[ $# -eq 1 -a "$1" == "-f" ] && INTERACTIVE=0
+[ $# -eq 1 ] && [ "$1" == "-f" ] && INTERACTIVE=0
 
 ask_bool() {
 	local default="$1"; shift;
@@ -28,27 +26,26 @@ ask_bool() {
 
 grep -q " ${ARCHDIR} fuse.sshfs " /proc/mounts || mount "${ARCHDIR}"
 
-set -v
+set -ev
 
-(
 _target=$( grep '^CONFIG_TARGET_[a-z0-9]\+=y' .config | sed -e 's:^CONFIG_TARGET_\([a-z0-9]\+\)=y:\1:' )
 _imgdir=$( ls -d bin/targets/"$_target"/* )
- eval $( grep CONFIG_EXTERNAL_KERNEL_TREE .config)
+eval "$( grep CONFIG_EXTERNAL_KERNEL_TREE .config )"
 
-_build=$( cat "$_imgdir"/version.buildinfo )
+# shellcheck disable=SC2012
 _img=$( ls -t "$_imgdir"/openwrt-*.img??? | head -1 )
+_build=$( cat "$_imgdir"/version.buildinfo )
 _date=$( date -r "$_img" +%Y%m%d_%H%M%S )
-_subdir=$_date-$(echo "$_img" | cut -d'-' -f3-4)
+_subdir=$_date-$( echo "$_img" | cut -d'-' -f3-4 )
 
-_destdir="$DESTDIR/${_subdir}"
-_archdir="$ARCHDIR/${_subdir}"
+_archdir="$ARCHDIR/$_target/$_subdir"
 
 _TAG="$_date.$_build.master-x64"
-_TAG2="$_date.$_build.asustek-computer-inc-sabertooth-z77"
+_TAG2="$_date.$_build.asus-rog-strix-z690-i-gaming-wifi"
 _pwd=$(pwd)
 
-if [ -d "$_destdir" ]; then
-	echo "$_destdir already exists"
+if [ -d "$_archdir" ]; then
+	echo "$_archdir already exists"
 	exit 1
 fi
 
@@ -57,28 +54,15 @@ for dir in . feeds/packages feeds/luci feeds/routing; do
 	ask_bool 1 "Add git tag $_TAG for $dir" && git tag "$_TAG"
 	ask_bool 1 "Push force and push new tag $_TAG for $dir" && ( git push --force origin && git push --tags origin )
 done
-cd "$_pwd"/files-asustek-computer-inc-sabertooth-z77
+cd "$_pwd"/files-asus-rog-strix-z690-i-gaming-wifi
 ask_bool 1 "Add git tag $_TAG2" && git tag "$_TAG2"
 ask_bool 1 "Push force and push new tag $_TAG2" && ( git push --force origin && git push --tags origin )
 cd "$_pwd"
 
-mkdir -p "$_destdir"
-cp -a -v bin/packages bin/targets "$_destdir"/
-[ -n "$CONFIG_EXTERNAL_KERNEL_TREE" ] && cp -a "$CONFIG_EXTERNAL_KERNEL_TREE"/.config "$_destdir"/targets/x86/64/config-kernel."${_TAG2}"
-
 mkdir -p "$_archdir"
-_t="$_destdir/${_imgdir/bin\/}"
-for file in $( ls "$_t" | grep "$ARCHIVE" ); do
-	echo "Moving $file to archive..."
-	mv "$_t/$file" "$_archdir"/
-done
+cp -a -v bin/packages bin/targets "$_archdir"/
+[ -n "$CONFIG_EXTERNAL_KERNEL_TREE" ] && cp -a "$CONFIG_EXTERNAL_KERNEL_TREE/.config" "$_archdir${_imgdir/bin}/config-kernel.$_TAG2"
 
-rm -f "$DESTDIR/latest" "$ARCHDIR/latest" &>/dev/null || true
-ln -s "$_subdir" "$DESTDIR/latest"
-ln -s "$_subdir" "$ARCHDIR/latest"
+rm -f "$( dirname "$_archdir" )/latest" &>/dev/null || true
+ln -s "$_subdir" "$( dirname "$_archdir" )/latest"
 
-)
-
-set +v
-
-exit 0
